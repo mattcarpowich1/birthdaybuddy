693af6ba511bd172eb69d9bdec1a1bcf31f1d055
Matt Carpowich <matthewcarpowich@Matts-MacBook-Pro.local>
2019-12-05 22:35:06 -0800

    inital commit for certs

---
+++ app/client/build/precache-manifest.efc1066f2e7799bd5b61caeccb768ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603af1f7c9b079cba247937abadb0667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04fc164da35833d1b7c3",
+    "url": "/static/css/main.18f79b2b.chunk.css"
+  },
+  {
+    "revision": "4362a4f312606a7c4f0e",
+    "url": "/static/js/2.d85fda0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d85fda0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04fc164da35833d1b7c3",
+    "url": "/static/js/main.0d04b33a.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "b088fd3d91a12563aaefd235832a6f35",
+    "url": "/static/media/clouds.b088fd3d.jpg"
+  }
+]);
---
+++ app/client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container" />
+  );
+}
+
+export default App;
